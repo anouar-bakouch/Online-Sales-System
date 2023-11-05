81181581b9b0d1d5cbd582f698a2b687e43bd678
anouar bakouch <anouar.bakouch@usmba.ac.ma>
2023-11-05 15:13:47 +0100

    api view for client and produit

--- systeme_vente_ligne/myservice/views.py
+++ systeme_vente_ligne/myservice/views.py
@@ -10,10 +10,67 @@ def index(request):
     return render(request,'index.html')
 
 # Client 
+class ClientAPIView(APIView):
+    """
+    API view for the `Client` model.
+    """
 
+    def get(self, request):
+        """
+        Get all clients.
+        """
+        clients = Client.objects.all()
+        serializer = ClientSerializer(clients, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        """
+        Create a new client.
+        """
+        serializer = ClientSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, pk):
+        """
+        Delete an existing client.
+        """
+        client = Client.objects.get(pk=pk)
+        client.delete()
+        return Response(status=204)
 
 # Produit 
+class ProduitAPIView(APIView):
+    """
+    API view for the `Produit` model.
+    """
+    def get(self, request):
+        """
+        Get all products.
+        """
+        products = Produit.objects.all()
+        serializer = ProduitSerializer(products, many=True)
+        return Response(serializer.data)
 
+    def post(self, request):
+        """
+        Create a new product.
+        """
+        serializer = ProduitSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, pk):
+        """
+        Delete an existing product.
+        """
+        product = Produit.objects.get(pk=pk)
+        product.delete()
+        return Response(status=204)
 
 # Commande 
 
--- systeme_vente_ligne/systeme_vente_ligne/urls.py
+++ systeme_vente_ligne/systeme_vente_ligne/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from myservice.views import index
+from myservice.views import index,ProduitAPIView,ClientAPIView
 
 urlpatterns = [
     path('',index),
     path('admin/', admin.site.urls),
+    path('api/produits/', ProduitAPIView.as_view(), name='produits'),
+    path('api/clients/',ClientAPIView.as_view(),name='clients'),
 ]
