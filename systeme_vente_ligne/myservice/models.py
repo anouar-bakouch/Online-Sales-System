a5db2ed469525869e6008174390c5d8cb01c331c
anouar bakouch <anouar.bakouch@usmba.ac.ma>
2023-11-05 14:03:39 +0100

    modelisation

---
+++ systeme_vente_ligne/myservice/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+# Create your models here.
+class Produit(models.Model):
+  id = models.AutoField(primary_key=True)
+  nom = models.CharField(max_length=255)
+  description = models.TextField()
+  prix = models.FloatField()
+
+class Commande(models.Model):
+  id = models.AutoField(primary_key=True)
+  date = models.DateTimeField()
+  montant = models.FloatField()
+  etat = models.CharField(max_length=255)
+  client = models.ForeignKey(client,on_delete=models.CASCADE)
+
+
+class Client(models.Model):
+  id = models.AutoField(primary_key=True)
+  nom = models.CharField(max_length=255)
+  email = models.EmailField()
+  adresses = models.ManyToManyField(Adresse)
+  modes_paiement = models.ManyToManyField(ModePaiement)
+  points_fidelite = models.IntegerField()
+
+class LigneCommande(models.Model):
+  id = models.AutoField(primary_key=True)
+  commande = models.ForeignKey(Commande, on_delete=models.CASCADE)
+  produit = models.ForeignKey(Produit, on_delete=models.CASCADE)
+  quantite = models.IntegerField()
+
+class Adresse(models.Model):
+  id = models.AutoField(primary_key=True)
+  nom = models.CharField(max_length=255)
+  prenom = models.CharField(max_length=255)
+  adresse = models.CharField(max_length=255)
+  code_postal = models.CharField(max_length=255)
+  ville = models.CharField(max_length=255)
+
+class ModePaiement(models.Model):
+  id = models.AutoField(primary_key=True)
+  nom = models.CharField(max_length=255)
+
+class RetourProduit(models.Model):
+  id = models.AutoField(primary_key=True)
+  produit = models.ForeignKey(Produit, on_delete=models.CASCADE)
+  etat = models.CharField(max_length=255)
+
